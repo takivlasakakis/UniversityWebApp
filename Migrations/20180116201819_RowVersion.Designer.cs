6995947207ac3e02515183a468e331b2a4ad6ada
Taki Vlasakakis <takivlasakakis@Takis-MacBook-Pro.local>
2018-01-16 14:16:43 -0700

    finish all views and controls

--- Migrations/20180116201819_RowVersion.Designer.cs
+++ Migrations/20180116201819_RowVersion.Designer.cs
@@ -12,9 +12,10 @@ using UniversityWebApp.Models;
 namespace UniversityWebApp.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20180116201819_RowVersion")]
+    partial class RowVersion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
