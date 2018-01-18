f3f9d43151a000d0d86cf0410833fc1cc123a868
Taki Vlasakakis <takivlasakakis@Takis-MacBook-Pro.local>
2018-01-18 10:26:41 -0700

    Add model inheritance

--- Migrations/20180116213243_Inheritance.Designer.cs
+++ Migrations/20180116213243_Inheritance.Designer.cs
@@ -13,9 +13,10 @@ using UniversityWebApp.Models;
 namespace UniversityWebApp.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20180116213243_Inheritance")]
+    partial class Inheritance
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
